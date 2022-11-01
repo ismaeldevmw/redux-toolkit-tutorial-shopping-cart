bbee6308e00dce830d10c3e86293ebbb34dc14e9
Ismael López <ismaellopez.dev@gmail.comm>
2022-10-31 22:18:18 -0600

    [ADD] reducers increase, decrease & toggle amount

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,6 @@
 import { ChevronUp, ChevronDown } from '../assets/icons'
 import { useDispatch } from 'react-redux'
-import { removeItem } from '../features/cart/cartSlice'
+import { removeItem, increase, decrease } from '../features/cart/cartSlice'
 
 const CartItem = ({ id, img, title, price, amount }) => {
   const dispatch = useDispatch()
@@ -11,14 +11,29 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         <h4 className='item-price'>{title}</h4>
         <h4 className='item-price'>${price}</h4>
-        <button className='remove-btn' onClick={() => dispatch(removeItem(id))}>remove</button>
+        <button className='remove-btn' onClick={() => {
+            dispatch(removeItem(id))
+          }}
+        >
+          remove
+        </button>
       </div>
       <div>
-        <button className='amount-btn'>
+        <button className='amount-btn' onClick={() => {
+            dispatch(increase({id}))
+          }}
+        >
           <ChevronUp />
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn'>
+        <button className='amount-btn' onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id))
+              return
+            }
+            dispatch(decrease({id}))
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
